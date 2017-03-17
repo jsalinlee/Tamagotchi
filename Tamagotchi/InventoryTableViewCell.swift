93407b33bed8fae0195e2874637e24d5b8a1720e
Justin Chang <jqchang@gmail.com>
2017-03-17 05:05:13 -0700

    Saving branch

--- Tamagotchi/InventoryTableViewCell.swift
+++ Tamagotchi/InventoryTableViewCell.swift
@@ -7,17 +7,69 @@
 //
 
 import UIKit
+import CoreData
 
 class InventoryTableViewCell: UITableViewCell {
     var cellDelegate: ActionButtonDelegate?
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var invList:Inventory?
+    
     @IBOutlet weak var itemLabel: UILabel!
     @IBOutlet weak var actionButtonLabel: UIButton!
     
     @IBAction func actionButtonPressed(_ sender: UIButton) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Inventory")
+        do {
+            let result = try managedObjectContext.fetch(request)
+            var item = result as! [Inventory]
+            if item.count == 0 {
+                let playerInv = Inventory(context: managedObjectContext)
+                playerInv.redCount = 0
+                playerInv.blueCount = 0
+                playerInv.greenCount = 0
+                playerInv.yellowCount = 0
+                playerInv.gojira = 0
+                playerInv.kendama = 0
+                playerInv.rubberBall = 0
+                do {
+                    try managedObjectContext.save()
+                } catch { print("Error") }
+                item = [playerInv]
+            }
+            // Assign the inventory instance to self
+            self.invList = item[0]
+        } catch {
+            let playerInv = Inventory(context: managedObjectContext)
+            playerInv.redCount = 0
+            playerInv.blueCount = 0
+            playerInv.greenCount = 0
+            playerInv.yellowCount = 0
+            playerInv.gojira = 0
+            playerInv.kendama = 0
+            playerInv.rubberBall = 0
+            do {
+                try managedObjectContext.save()
+            } catch { print("Error") }
+        }
+        
         self.cellDelegate?.actionDelegateButtonPressed()
         let str = self.itemLabel!.text
         let end = str!.index(str!.startIndex, offsetBy: 1)
-        print("Action button pressed - \(str!.substring(to: end))")
+        switch(str!.substring(to: end)) {
+        case "R":
+            if let red = invList?.redCount {
+                print("Red berries \(red)")
+                invList?.redCount -= 1
+            }
+        case "B":
+            print("Blue berry button pressed!")
+        case "G":
+            print("Green berry button pressed!")
+        case "Y":
+            print("Yellow berry button pressed!")
+        default:
+            print("Unknown")
+        }
     }
     override func awakeFromNib() {
         super.awakeFromNib()
