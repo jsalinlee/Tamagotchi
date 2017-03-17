7cf142ef05c0a57ed8b41dd42c4f7b67db5f0171
Justin Chang <jqchang@gmail.com>
2017-03-16 18:48:33 -0700

    Added player inventory CoreData access

--- Tamagotchi/InventoryTableViewController.swift
+++ Tamagotchi/InventoryTableViewController.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class InventoryTableViewController: UITableViewController {
     var itemSections = ["Food", "Toys"]
     var food = ["Red Berry", "Blue Berry", "Green Berry", "Purple Berry"]
+    var foodCounts = [Int32]()
     var toys = ["Red rubber ball", "Kendama", "Gojira Action Figure"]
+    var toyCounts = [Int32]()
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var inventoryList:Inventory?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fetchAllItems()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -21,7 +28,46 @@ class InventoryTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
+    func fetchAllItems() {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Inventory")
+        do {
+            // If inventory does not exist, initialize empty inventory
+            let result = try managedObjectContext.fetch(request)
+            let item = result as! [Inventory]
+            if item.count == 0 {
+                let playerInv = Inventory(context: managedObjectContext)
+                playerInv.redCount = 0
+                playerInv.blueCount = 0
+                playerInv.greenCount = 0
+                playerInv.yellowCount = 0
+                playerInv.gojira = 0
+                playerInv.kendama = 0
+                playerInv.rubberBall = 0
+                do {
+                    try managedObjectContext.save()
+                } catch { print("Error") }
+            }
+            // Assign the inventory instance to self
+            self.inventoryList = item[0]
+            self.foodCounts = [(inventoryList?.redCount)!, (inventoryList?.blueCount)!, (inventoryList?.greenCount)!, (inventoryList?.yellowCount)!]
+            self.toyCounts = [(inventoryList?.rubberBall)!, (inventoryList?.kendama)!, (inventoryList?.gojira)!]
+        } catch {
+            let playerInv = Inventory(context: managedObjectContext)
+            playerInv.redCount = 3
+            playerInv.blueCount = 3
+            playerInv.greenCount = 3
+            playerInv.yellowCount = 3
+            playerInv.gojira = 3
+            playerInv.kendama = 3
+            playerInv.rubberBall = 3
+            do {
+                try managedObjectContext.save()
+            } catch { print("Error") }
+        }
+        
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
@@ -39,9 +85,9 @@ class InventoryTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "InventoryCell", for: indexPath)
         if indexPath.section == 0 {
-            cell.textLabel?.text = food[indexPath.row]
+            cell.textLabel?.text = "\(food[indexPath.row]): \(foodCounts[indexPath.row])"
         } else if indexPath.section == 1 {
-            cell.textLabel?.text = toys[indexPath.row]
+            cell.textLabel?.text = "\(toys[indexPath.row]): \(toyCounts[indexPath.row])"
         }
         return cell
     }
